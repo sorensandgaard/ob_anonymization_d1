4b1262c4da163cea7a7341fcb1e2b1507015b518
Søren Sandgaard <sorsa@dtu.dk>
2024-11-11 14:17:28 +0100

    Changed name of output

--- entrypoint_data.py
+++ entrypoint_data.py
@@ -19,14 +19,16 @@ def materialize_dataset(output_dir, name):
     R1_url = "https://github.com/sorensandgaard/ob_anonymization_dataloss_d1/blob/main/TEST_S1_L001_R1_001.fastq.gz"
     R2_url = "https://github.com/sorensandgaard/ob_anonymization_dataloss_d1/blob/main/TEST_S1_L001_R2_001.fastq.gz"
 
-    R1_reads = "TEST_S1_L001_R1_001.fastq.gz"
-    R2_reads = "TEST_S1_L001_R2_001.fastq.gz"
+#    R1_reads = "TEST_S1_L001_R1_001.fastq.gz"
+#    R2_reads = "TEST_S1_L001_R2_001.fastq.gz"
 
     # Create dataset files
-    data_R1_file = os.path.join(output_dir, R1_reads)
-    data_R2_file = os.path.join(output_dir, R2_reads)
+#    data_R1_file = os.path.join(output_dir, R1_reads)
+#    data_R2_file = os.path.join(output_dir, R2_reads)
 
-    print("Testing whether I can print to the snakemake terminal")
+    data_R1_file = os.path.join(output_dir, f'{name}.R1.fastq.gz')
+    data_R2_file = os.path.join(output_dir, f'{name}.R2.fastq.gz')
+
     create_file(data_R1_file,R1_url)
     create_file(data_R2_file,R2_url)
 
